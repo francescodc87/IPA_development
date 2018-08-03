99f210f285cefc5ca71f8d46dc3c28c8ac2fb8ff
ouyangyuqi <yuqi.ouyang@postgrad.manchester.ac.uk>
2018-08-03 19:35:52 +0100

    .

--- RworkingDirectory/RcppTest/GSRcppApply.R
+++
@@ -1,5 +0,0 @@
-# function to sample from a defined uniform probability distribution
-"RcppAsample" <- function(P){
-  o <- Rcppsample(1:length(P), size = 1, replace = TRUE, P)
-  o
-}
--- RworkingDirectory/RcppTest/GibbsSamplerR.R
+++
@@ -1,6 +0,0 @@
-# function to sample from a defined uniform probability distribution
-"Rsample" <- function(P){
-  o <- sample(1:length(P), size = 1, replace = TRUE, prob=P)
-  o
-}
-
